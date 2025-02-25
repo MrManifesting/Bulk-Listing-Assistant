9495ed293b857c70e6ea408ff64c56ccc95ac2f6
NICHOLAS KELLEY <WHATEVERSCLEVERBOSS@GMAIL.COM>
2025-02-25 07:50:31 -0800

    Initial Commit

---
+++ Bulk Listing AssistantTests/Bulk_Listing_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bulk_Listing_AssistantTests.swift
+//  Bulk Listing AssistantTests
+//
+//  Created by Nick Kelley on 2/25/25.
+//
+
+import Testing
+@testable import MR_LISTER_Inventory_Bulk_Listing_Assistant
+
+struct Bulk_Listing_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Bulk Listing AssistantUITests/Bulk_Listing_AssistantUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bulk_Listing_AssistantUITestsLaunchTests.swift
+//  Bulk Listing AssistantUITests
+//
+//  Created by Nick Kelley on 2/25/25.
+//
+
+import XCTest
+
+final class Bulk_Listing_AssistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
